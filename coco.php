dde49f8b97062079f04e7de1343d0a75b6a99522
sergio-ccz <sergio.castro@gmx.com>
2011-11-26 18:20:39 -0800

    profile pic arreglado

---
+++ coco.php
@@ -0,0 +1,10 @@
+<?php require_once("includes/session.php"); ?>
+<?php require_once("Includes/connection.php"); ?>
+<?php require_once("Includes/functions.php"); ?>
+<?php include("includes/header.php"); ?>
+
+<?php 
+	get_user_pic(8);
+?>
+
+<?php require("Includes/footer.php"); ?>
